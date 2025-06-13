9c3624a94e61795468f843f9beff89a30f2a3dec
Nilay Vishwakarma <nivishwa@microsof.com>
2025-06-13 16:13:16 +0530

    push new page

---
+++ astro.config.mjs
@@ -0,0 +1,47 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "kokaq",
+      logo: {
+        replacesTitle: true,
+        light: "./public/kokaq-logo-light.png",
+        dark: "./public/kokaq-logo-dark.png",
+      },
+      customCss: ["./src/styles/docs.css"],
+      // useStarlightDarkModeSwitch: false,
+      favicon: "/favicon.ico",
+      editLink: {
+        baseUrl: "https://github.com/kokaq/kokaq.github.io/",
+      },
+      lastUpdated: true,
+      expressiveCode: {
+        textMarkers: true,
+        themes: ["ayu-dark", "light-plus"],
+        defaultProps: {
+          wrap: true,
+        },
+        styleOverrides: {
+          borderRadius: "0.2rem",
+        },
+      },
+      sidebar: [
+        {
+          label: "Get started",
+          autogenerate: { directory: "get-started" },
+        },
+        {
+          label: "SDK",
+          autogenerate: { directory: "sdk" },
+        },
+        {
+          label: "Commands",
+          autogenerate: { directory: "commands" },
+        },
+      ],
+    }),
+  ],
+});
---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,6 @@
+---
+title: "Hello, World"
+description: ""
+published_at: 2025-06-13
+author: nilay
+---
